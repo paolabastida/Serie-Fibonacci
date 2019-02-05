167b832bdf8e9a1f3575f3640d8d092f5c024f9a
Omar Flores Salazar <31521501+HangingEmperor@users.noreply.github.com>
2019-02-05 13:04:16 -0800

    Initial commit

---
+++ src/Recursividad.java
@@ -0,0 +1,13 @@
+public class Recursividad {
+    public static int funcionFibonacci(int num){
+        if(num == 0 || num==1)
+            return num;
+        else
+            return funcionFibonacci(num-1) + funcionFibonacci(num-2);
+    }
+
+    public static void main(String[] args) {
+        funcionFibonacci(6);
+        System.out.println("Numero Fibonacci"+ " "+funcionFibonacci(6));
+    }
+}
